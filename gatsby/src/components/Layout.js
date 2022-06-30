5e3cd310af62136c57107196d4dd32802f675c30
Marcio Morgado <marciomorgado@live.ca>
2022-06-30 14:41:15 -0400

    added viewport tag

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,31 +1,28 @@
-import React from "react";
-import { Helmet } from "react-helmet";
-import Footer from "./Footer";
-import Nav from "./Nav";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Footer from './Footer';
+import Nav from './Nav';
 
-import "sanitize.css";
-import GlobalStyle from "../styles/GlobalStyles";
-import Typography from "../styles/Typography";
+import 'sanitize.css';
+import GlobalStyle from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children, breadcrumbs }) {
-    return (
-        <>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>
-                    Marcio Morgado{" "}
-                    {breadcrumbs ? ` - ${breadcrumbs.join(" - ")}` : ``}
-                </title>
-            </Helmet>
+   return (
+      <>
+         <Helmet>
+            <title>Marcio Morgado {breadcrumbs ? ` - ${breadcrumbs.join(' - ')}` : ``}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1" />
+         </Helmet>
 
-            <GlobalStyle />
-            <Typography />
+         <GlobalStyle />
+         <Typography />
 
-            <Nav />
+         <Nav />
 
-            <main> {children} </main>
+         <main> {children} </main>
 
-            <Footer />
-        </>
-    );
+         <Footer />
+      </>
+   );
 }
